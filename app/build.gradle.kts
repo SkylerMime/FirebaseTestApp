164a0b23b4f544891e926984ae5e4487f2ef5d03
Skyler Atchison <skyler.atchison145@myci.csuci.edu>
2023-09-17 10:37:58 -0700

    can send and receive data with firebase!!!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    // Google services gradle plugin
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -60,6 +62,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-common-ktx:20.3.3")
+    implementation("com.google.firebase:firebase-crashlytics-buildtools:2.9.9")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -67,4 +71,8 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    // Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    // realtime database library
+    implementation("com.google.firebase:firebase-database-ktx:20.2.2")
 }
--- app/src/main/java/com/example/firebasetestapp/MainActivity.kt
+++ app/src/main/java/com/example/firebasetestapp/MainActivity.kt
@@ -2,13 +2,18 @@ package com.example.firebasetestapp
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import android.widget.Button
 import android.widget.Toast
-import com.example.firebasetestapp.R
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : Activity() {
-    private var numberToSend: Int = 0
+    private val TAG = "MainActivity"
+    private var numberToSend: Long = 0
 
     private fun updateNumber(increment: Boolean) {
         if (increment) {
@@ -25,6 +30,27 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("currentCount")
+
+        // read from the database
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // this method is called once with the initial value and again
+                // whenever data at this location is updated.
+                numberToSend = dataSnapshot.getValue() as Long
+                val messageBox: TextView = findViewById(R.id.countMessage)
+                messageBox.text = numberToSend.toString()
+                val logMessage = numberToSend.toString()
+                Log.d(TAG, "Value is: $logMessage")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
+
         val incrementButton: Button = findViewById(R.id.incrementButton)
         incrementButton.setOnClickListener {
             updateNumber(true)
@@ -37,6 +63,9 @@ class MainActivity : Activity() {
 
         val sendButton: Button = findViewById(R.id.databaseSendButton)
         sendButton.setOnClickListener {
+            // send the current count to the database
+            myRef.setValue(numberToSend)
+
             val text = getString(R.string.message_sent)
             val toast = Toast.makeText(this, text, Toast.LENGTH_SHORT)
             toast.show()
